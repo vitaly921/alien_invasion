8e96303e917abfe93fe1bee05f2cb9cf845e2548
Виталий Приходьков <prihodkov21vek@gmail.com>
2024-06-14 00:26:46 +0300

    Improved code and added comments in the Bullet, Shape Bullet, Alien Bullet, Explosion, Small Explosion classes; Updated the image of the alien ship's bullet.

--- bullet.py
+++ bullet.py
@@ -5,7 +5,7 @@ from pygame.sprite import Sprite
 class Bullet(Sprite):
     """Базовый класс для управления пулями корабля игрока и пришельца"""
     def __init__(self, ai_settings, screen, game_ship):
-        """"""
+        """Инициализация атрибутов базового класса"""
         super().__init__()
         self.screen = screen
         self.game_ship = game_ship
@@ -14,42 +14,56 @@ class Bullet(Sprite):
         self.rect = None
 
     def draw_bullet(self):
+        """Функция для отрисовки пули на экране"""
         pygame.draw.rect(self.screen, self.color, self.rect)
 
 
 class ShipBullet(Bullet):
-    """"""
+    """Класс для управления пулями корабля игрока"""
     def __init__(self, ai_settings, screen, ship):
         super().__init__(ai_settings, screen, ship)
+        # создание изображения пули в виде прямоугольника
         self.rect = pygame.Rect(0, 0, ai_settings.bullet_width_for_ship, ai_settings.bullet_height_for_ship)
+        # задание расположения пули относительно корабля игрока
         self.rect.centerx = ship.rect.centerx
         self.rect.top = ship.rect.top
+        # задание цвета пули
         self.color = ai_settings.bullet_color_for_ship
+        # преобразование координаты y в вещественное число
         self.y = float(self.rect.y)
 
     def update(self):
+        """Функция для обновления местоположения пули"""
+        # закон изменения координаты y
         self.y -= self.speed_factor
+        # присвоение пули обновленной вещественной координаты y
         self.rect.y = self.y
 
 
 class AlienBullet(Bullet):
-    """"""
+    """Класс для управления пулями кораблей пришельцев"""
     def __init__(self, ai_settings, screen, alien):
         super().__init__(ai_settings, screen, alien)
+        # задание изображения пули пришельца, его размера и конвертация
         self.image = pygame.image.load('images/alien_bullet.png')
         self.image = pygame.transform.scale(self.image, (22, 45))
         self.image = self.image.convert_alpha()
+        # получение прямоугольника изображения
         self.rect = self.image.get_rect()
+        # задание расположения пули относительно корабля пришельца
         self.rect.centerx = alien.rect.centerx
         self.rect.bottom = alien.rect.bottom
+        # преобразование координаты y в вещественное число
         self.y = float(self.rect.y)
 
     def update(self):
-        """"""
+        """Функция для обновления местоположения пули"""
+        # закон изменения координаты y
         self.y += self.speed_factor / 2
+        # присвоение пули обновленной вещественной координаты y
         self.rect.y = self.y
 
     def draw_bullet(self):
+        """Функция для отрисовки изображения пули на экране"""
         self.screen.blit(self.image, self.rect)
-
 
--- explosion.py
+++ explosion.py
@@ -3,20 +3,13 @@ from pygame.sprite import Sprite
 
 
 class Explosion(Sprite):
-    """Наследуемый класс для создания экземпляра эффекта взрыва"""
-
+    """Базовый класс для создания экземпляра эффекта взрыва"""
     def __init__(self, ai_settings, screen, game_ship, for_alien=False):
         """Инициализация атрибутов"""
         super().__init__()
         self.game_ship = game_ship
         self.for_alien = for_alien
         self.ai_settings = ai_settings
-        if self.for_alien:
-            self.pos_x = game_ship.x
-            self.pos_y = game_ship.y
-        else:
-            self.pos_x = game_ship.centerx - 25
-            self.pos_y = game_ship.centery - 25
         self.screen = screen
         self.screen_rect = screen.get_rect()
 
@@ -27,65 +20,53 @@ class Explosion(Sprite):
         self.image = self.image.convert_alpha()
         self.rect = self.image.get_rect()
 
-        self.creation_time = pygame.time.get_ticks()
-        #self.explosion_duration = 350
-
-        # задание координат для прямоугольника изображения
-        self.rect.x = self.pos_x
-        self.rect.y = self.pos_y
-        # конвертация координаты Х в число с плавающей точкой
-        self.x = float(self.rect.x)
-        self.y = float(self.rect.y)
-
-    def update(self):
-        """Функция обновления координат взрыва"""
-        if self.for_alien:
-            # подсчет координаты Х взрыва, соответствущей координате уничтоженного корабля в формате float
-            self.x += (self.ai_settings.alien_speed_factor * self.ai_settings.fleet_direction)
-            # передача посчитанной координаты Х прямоугольнику изображения взрыва в формате int
-            self.rect.x = self.x
-        else:
-            self.pos_x = self.game_ship.centerx
-            self.pos_y = self.game_ship.centery + 45
-
-    def blitme(self):
-        """Отрисовка пришельца"""
-        self.screen.blit(self.image, self.rect)
-
-
-class SmallExplosion(Explosion):
-    """"""
-
-    def __init__(self, ai_settings, screen, game_ship, for_alien=False):
-        """"""
-        super().__init__(ai_settings, screen, game_ship, for_alien)
-        self.image = pygame.transform.scale(self.image, (25, 25))
-        self.rect = self.image.get_rect()
+        # сохранение координат места взрыва у корабля игрока или пришельца
         if self.for_alien:
             self.pos_x = game_ship.x
             self.pos_y = game_ship.y
         else:
-            self.pos_x = game_ship.centerx
-            self.pos_y = game_ship.centery + 45
+            self.pos_x = game_ship.centerx - 25
+            self.pos_y = game_ship.centery - 25
 
-        # задание координат для прямоугольника изображения
+        # задание координат взрыва на основе полученных координат мест взрыва
         self.rect.x = self.pos_x
         self.rect.y = self.pos_y
-        # конвертация координаты Х в число с плавающей точкой
+        # конвертация координат Х и Y в числа с плавающей точкой
         self.x = float(self.rect.x)
         self.y = float(self.rect.y)
 
     def update(self):
-        if self.for_alien:
-            # подсчет координаты Х взрыва, соответствущей координате уничтоженного корабля в формате float
-            self.x = self.game_ship.rect.centerx - 12
-            # передача посчитанной координаты Х прямоугольнику изображения взрыва в формате int
-            self.y = self.game_ship.rect.bottom - 8
-            self.rect.x = self.x
-            self.rect.y = self.y
+        """Функция обновления координат взрыва в процессе игры"""
+        # подсчет координаты Х взрыва, соответствущей координате уничтоженного корабля в формате float
+        self.x += (self.ai_settings.alien_speed_factor * self.ai_settings.fleet_direction)
+        # передача посчитанной координаты Х прямоугольнику изображения взрыва в формате int
+        self.rect.x = self.x
 
+    def blitme(self):
+        """Отрисовка эффекта взрыва на экране"""
+        self.screen.blit(self.image, self.rect)
+
+
+class SmallExplosion(Explosion):
+    """Наследуемый класс для создания мини-эффекта взрыва """
+    def __init__(self, ai_settings, screen, game_ship, for_alien=False):
+        """Инициализация атрибутов"""
+        super().__init__(ai_settings, screen, game_ship, for_alien)
+        # изменение размеров изображения, конвертация, обновление прямоугольника
+        self.image = pygame.transform.scale(self.image, (25, 25))
+        self.image = self.image.convert_alpha()
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        """Функция обновления координат мини-взрыва в процессе игры"""
+        if self.for_alien:
+            # закон изменения координат мини-взрыва относительно корабля пришельца
+            self.x = self.game_ship.rect.centerx - 12
+            self.y = self.game_ship.rect.bottom - 8
         else:
+            # закон изменения координат мини-взрыва относительно корабля игрока
             self.x = self.game_ship.centerx - 13
             self.y = self.game_ship.centery - 60
-            self.rect.x = self.x
-            self.rect.y = self.y
+        # передача посчитанных координат прямоугольнику изображения взрыва в формате int
+        self.rect.x = self.x
+        self.rect.y = self.y
